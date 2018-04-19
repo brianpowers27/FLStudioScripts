22ad285606bdaaba17ce899e2c8e6a72b87e0fc6
Brian Powers <brianpowers27@gmail.com>
2018-04-19 07:20:23 -0400

    so many bugfix

--- export new flp projects.ps1
+++ export new flp projects.ps1
@@ -4,6 +4,7 @@ $WorkingDirectory='H:\Program Files (x86)\Image-Line\FL Studio 12\Data\Projects\
 # for each file in directory
 $files = Get-ChildItem $WorkingDirectory -Filter *.flp 
 $TempFolder = 'H:\Program Files (x86)\Image-Line\FL Studio 12\Data\Projects\bdp\renderTemp'
+$GoogleDriveFolder = 'C:\Users\brian\Google Drive\FL Studio Song Share\SongsByBDP'
 
 # try to create the subdirectory
 Remove-Item –path $TempFolder –recurse #clean old
@@ -26,14 +27,19 @@ foreach($file in $files) {
     $mp3File = Get-ItemProperty -Path $mp3FilePath -Name LastWriteTime -ErrorAction SilentlyContinue
 
     try{$mp3Writedate = ($mp3File.LastWriteTime).AddMinutes(20)} 
-    catch{}
+    catch
+    {
+        # no mp3 found, always write.
+        $mp3Writedate= Get-Date 
+        $mp3Writedate=$mp3Writedate.AddDays(-7000)
+    }
     
     
     # if wave time less than mp3 -  export
     if($mp3Writedate -le $projectWritedate)
     {
 		Copy-Item $flpFilePath $TempFolder
-        "Rendering --------------->"+$flpProjectName+" -- Process FLP file"
+        "         Rendering --------------->"+$flpProjectName+" -- Process FLP file"
         $commandToExecute= '"H:\Program Files (x86)\Image-Line\FL Studio 12\FL64.exe" /R /Emp3 /F"'+$TempFolder+'"'
         $argumentList= '/R /Emp3 /F"'+$TempFolder+'"'
         $processToExecute= '"H:\Program Files (x86)\Image-Line\FL Studio 12\FL.exe" /R /Emp3 /F"'+$TempFolder+'"' # was fl64
@@ -43,15 +49,29 @@ foreach($file in $files) {
         # $p.HasExited
         # $p.ExitCode
 
-        $newFile = $TempFolder+"\"+$flpProjectName+".mp3"
-        Copy-Item $newFile $WorkingDirectory
-        "Item Copied from "+$newFile+" to " +$WorkingDirectory+"\n"
+        #Copy to work dir
+        $newFileTemp = $TempFolder+"\"+$flpProjectName+".mp3"
+        Copy-Item $newFileTemp $WorkingDirectory
+        "      --Item Copied from "+$newFile
+        "      to " +$WorkingDirectory+"\n"
+        "--"
+        ""
+
+        #Copy to Google Drive
+        $newFileGoogleDrive = $GoogleDriveFolder+"\"+$flpProjectName+".mp3"
+        
+        Copy-Item $newFileTemp $GoogleDriveFolder -force
+        "      --Item Copied from "+$newFile
+        "      to " +$GoogleDriveFolder+"\n"
+        "--"
+        ""
+
         $TempFolderFile = $TempFolder+"\*"
         Remove-Item –path $TempFolderFile
     } 
     else
     {
-        $flpProjectName+ " -- Skipping  - FLP file is already up to date"
+        " ---------"+$flpProjectName+ " -- Skipping  - FLP file is already up to date"        
     }
 	
 
